e7d6faa912018268c2098551a1ba5dc95bc58f26
PabloL2003 <125266074+PabloL2003@users.noreply.github.com>
2024-12-06 16:51:08 +0100

    Start of frustrum
    Editing typesh to add plaues to create the frustrum culling

--- MyGameMaker/MyGameEngine/types.h
+++ MyGameMaker/MyGameEngine/types.h
@@ -28,3 +28,30 @@ typedef unsigned long ulong;
 typedef unsigned __int16 uint16;
 typedef unsigned __int32 uint32;
 typedef unsigned __int64 uint64;
+
+
+
+struct Point {
+	vec3 p;
+	Point(const vec3& v3) : p(v3) {}
+};
+
+inline Point operator*(const mat4& m, const Point& p) {
+	const auto v3 = m * vec4(p.p, 1);
+	return Point(vec3(v3) / v3.w);
+}
+
+struct Vector {
+	vec3 v;
+	Vector(const vec3& v3) : v(v3) {}
+};
+
+inline Vector operator*(const mat4& m, const Vector& v) { return Vector(vec3(m * vec4(v.v, 0))); }
+
+struct Plane {
+	Vector normal;
+	Point point;
+	Plane(const Vector& normal, const Point& point) : normal(normal), point(point) {}
+};
+
+inline Plane operator*(const mat4& m, const Plane& plane) { return Plane(m * plane.normal, m * plane.point); }
